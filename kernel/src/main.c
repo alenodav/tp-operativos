19906b21c00e82c4ec739a242fd9c062d3e9126d
tomasp0603 <tommpe.03@gmail.com>
2025-04-10 22:17:14 +0000

    Se agregan funciones y structs en el utils (a probar)

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,6 +1,29 @@
-#include <utils/hello.h>
+#include<main.h>
 
 int main(int argc, char* argv[]) {
-    saludar("kernel");
+    t_config *config = config_create("kernel.config");
+    if(config == NULL) {
+        abort();
+    }
+    t_log_level log_level = log_level_from_string(config_get_string_value(config, "LOG_LEVEL"));
+    if (log_level == -1) {
+        abort();
+    }
+    t_log *logger = log_create("kernel.log", "KERNEL", true, log_level);
+    if (logger == NULL) {
+        abort();
+    }
+    log_debug(logger, "Config y Logger creados correctamente.");
+
+    uint32_t fd_escucha_dispatch = iniciar_servidor(config_get_string_value(config, "PUERTO_ESCUCHA_DISPATCH"));
+
+    while (1) {
+        pthread_t thread;
+        int *fd_conexion_dispatch = malloc(sizeof(int));
+        *fd_conexion_dispatch = accept(fd_escucha_dispatch, NULL, NULL);
+        pthread_create(&thread, NULL, (void*) atender_cliente, fd_conexion_dispatch);
+        pthread_detach(thread);
+    }    
+
     return 0;
 }
---
+++ kernel/src/main.h
@@ -0,0 +1,6 @@
+#include <utils/hello.h>
+#include <utils/conexion.h>
+#include <utils/paquete.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include <pthread.h>
