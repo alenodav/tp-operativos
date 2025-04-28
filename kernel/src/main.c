8a8a66d7f16c7a9a62d13e8142cc6c43474a4e77
tomasp0603 <tommpe.03@gmail.com>
2025-04-28 20:55:33 +0000

    Se agrega funcionalidad inicial de planificador a largo plazo con FIFO

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,29 +1,46 @@
 #include<main.h>
-t_log *logger;
 
 int main(int argc, char* argv[]) {
-    t_config *config = crear_config("kernel");
+    config = crear_config("kernel");
     logger = crear_log(config, "kernel");
     log_debug(logger, "Config y Logger creados correctamente.");
 
-    //uint32_t fd_escucha_dispatch = iniciar_servidor(config_get_string_value(config, "PUERTO_ESCUCHA_DISPATCH"));
+    kernel_to_memoria *archivo_inicial = malloc(sizeof(kernel_to_memoria));
+    archivo_inicial->archivo = argv[1];
+    archivo_inicial->archivo_length = string_length(archivo_inicial->archivo) + 1;
+    archivo_inicial->tamanio = atoi(argv[2]);
+    cola_new = list_create();
+    cola_ready = list_create();
+    cola_blocked = list_create();
+    
+    // pthread_t thread_io;
+    // pthread_create(&thread_io, NULL, escucha_io, config); // Se crea un thread que corra escucha_io(config)
+    // pthread_detach(thread_io);  // Se separa la ejecución del thread de la del programa principal
 
-    pthread_t thread_io;
-    pthread_create(&thread_io, NULL, escucha_io, config); // Se crea un thread que corra escucha_io(config)
-    pthread_detach(thread_io);  // Se separa la ejecución del thread de la del programa principal
+    // pthread_t thread_memoria;
+    // pthread_create(&thread_memoria, NULL, handshake_memoria, config);
+    // pthread_detach(thread_memoria); 
 
-    pthread_t thread_memoria;
-    pthread_create(&thread_memoria, NULL, handshake_memoria, config);
-    pthread_detach(thread_memoria); 
+    // //sleep(1);
 
-    //sleep(1);
-
-    //Creo un hilo para que corra escucha_cpu(config)
-    pthread_t thread_cpu;
-    pthread_create(&thread_cpu, NULL, escucha_cpu, config);
-    pthread_detach(thread_cpu);
+    // //Creo un hilo para que corra escucha_cpu(config)
+    // pthread_t thread_cpu;
+    // pthread_create(&thread_cpu, NULL, escucha_cpu, config);
+    // pthread_detach(thread_cpu);
 
     getchar(); // Para que el progrma no termine antes que los threads
+
+    pthread_t planificador_largo_plazo;
+    pthread_create(&planificador_largo_plazo, NULL, (void*)largo_plazo, (void*)archivo_inicial);
+    pthread_join(planificador_largo_plazo, NULL);
+
+    // pthread_t administrador_dispositivos;
+    // pthread_create(&administrador_dispositivos, NULL, (void*)administrar_dispositivos, NULL);
+    // pthread_join(administrador_dispositivos, NULL);
+
+    list_destroy(cola_new);
+    list_destroy(cola_ready);
+    list_destroy(cola_blocked);
 
     config_destroy(config);
     log_info(logger, "Finalizo el proceso.");
@@ -32,7 +49,7 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-void escucha_io(t_config* config){
+void escucha_io(){
     uint32_t fd_escucha_io = iniciar_servidor(config_get_string_value(config, "PUERTO_ESCUCHA_IO"));    
     uint32_t socket_io = esperar_cliente(fd_escucha_io);
     char *identificador = recibir_handshake(socket_io);
@@ -48,9 +65,9 @@ void escucha_io(t_config* config){
     liberar_conexion(fd_escucha_io);
     return;
 }
-//prueba commit
+
 //Creo escucha cpu
-void escucha_cpu(t_config* config){
+void escucha_cpu(){
     uint32_t fd_escucha_cpu = iniciar_servidor(config_get_string_value(config, "PUERTO_ESCUCHA_DISPATCH"));    
     uint32_t socket_cpu = esperar_cliente(fd_escucha_cpu);
     char *identificador = recibir_handshake(socket_cpu);
@@ -68,7 +85,7 @@ void escucha_cpu(t_config* config){
 }
 
 
-void handshake_memoria(t_config *config) {
+void handshake_memoria() {
     uint32_t fd_conexion_memoria = crear_socket_cliente(config_get_string_value(config, "IP_MEMORIA"), config_get_string_value(config, "PUERTO_MEMORIA"));
 
     enviar_handshake(fd_conexion_memoria, "KERNEL");
@@ -84,4 +101,103 @@ void handshake_memoria(t_config *config) {
     free(identificador);
     liberar_conexion(fd_conexion_memoria);
     return;
+}
+
+void largo_plazo(kernel_to_memoria* archivo) {
+    //Seleccion de algoritmo
+    char* algoritmo_config = config_get_string_value(config, "ALGORITMO_COLA_NEW");
+    if (string_equals_ignore_case(algoritmo_config, "FIFO")) {
+        planificar_fifo(archivo);
+    }
+    else if (string_equals_ignore_case(algoritmo_config, "PMCP")) {
+
+    }
+    else {
+        log_error(logger, "Algoritmo desconocido para planificador de largo plazo.");
+    }
+    return;
+}
+
+void planificar_fifo(kernel_to_memoria* archivo) {
+    t_pcb *pcb = crear_proceso(archivo);
+    bool consulta_memoria = consultar_a_memoria(archivo->tamanio, pcb->pid);
+    if (consulta_memoria) {
+        enviar_instrucciones(archivo);
+        pasar_ready();
+    }
+    else {
+        log_debug(logger, "## (%d) No hay espacio suficiente para inicializar el proceso", pcb->pid);
+    }
+}
+
+t_estado_metricas *crear_metrica_estado(t_estado estado) {
+    t_estado_metricas *metrica_agregar = malloc(sizeof(t_estado_metricas));
+    metrica_agregar->estado = estado;
+    metrica_agregar->ME = 1;
+    metrica_agregar->MT = temporal_create();
+    return metrica_agregar;
+}
+
+t_pcb* crear_proceso(kernel_to_memoria* archivo) {
+    t_pcb *pcb = malloc(sizeof(t_pcb));
+    pcb->pid = pid_counter;
+    pcb->pc = 0;
+    pcb->metricas = list_create();
+    t_estado_metricas *new = crear_metrica_estado(NEW);
+    list_add(pcb->metricas, new);
+    pcb->estado_actual = NEW;
+    list_add(cola_new, pcb);
+    archivo->pid = pid_counter;
+    pid_counter += 1;
+    return pcb;
+}
+
+bool consultar_a_memoria(uint32_t tamanio_proceso, uint32_t pid) {
+    bool ret = false;
+    uint32_t fd_conexion_memoria = crear_socket_cliente(config_get_string_value(config, "IP_MEMORIA"), config_get_string_value(config, "PUERTO_MEMORIA"));
+    t_buffer* buffer = buffer_create(sizeof(uint32_t));
+    buffer_add_uint32(buffer, tamanio_proceso);
+    t_paquete* paquete = crear_paquete(CONSULTA_MEMORIA_PROCESO, buffer);
+    enviar_paquete(paquete, fd_conexion_memoria);
+    t_paquete *retorno = recibir_paquete(fd_conexion_memoria);
+    if (retorno->codigo_operacion == CONSULTA_MEMORIA_PROCESO) {
+        uint8_t retorno = buffer_read_uint8(paquete->buffer);
+        if (retorno != -1) {
+            ret = true;
+        }
+    }
+    else {
+        log_error(logger, "## (%d) Codigo de operación incorrecto para consultar a memoria.", pid);
+    }
+    free(retorno);
+    liberar_conexion(fd_conexion_memoria);
+    return ret;
+}
+
+void enviar_instrucciones(kernel_to_memoria* archivo) {
+    uint32_t fd_conexion_memoria = crear_socket_cliente(config_get_string_value(config, "IP_MEMORIA"), config_get_string_value(config, "PUERTO_MEMORIA"));
+    t_buffer *buffer = serializar_t_archivo(archivo);
+    t_paquete *consulta = crear_paquete(SAVE_INSTRUCTIONS, buffer);
+    enviar_paquete(consulta, fd_conexion_memoria);
+    liberar_conexion(fd_conexion_memoria);
+    free(archivo);
+    return;
+}
+
+t_buffer *serializar_kernel_to_memoria(kernel_to_memoria* archivo) {
+    t_buffer* buffer = buffer_create(sizeof(uint32_t) * 2 + archivo->archivo_length);
+    buffer_add_string(buffer, archivo->archivo_length, archivo->archivo);
+    buffer_add_uint32(buffer, archivo->archivo_length);
+    buffer_add_uint32(buffer, archivo->tamanio);
+    return buffer;
+}
+
+void pasar_ready() {
+    t_pcb *pcb = list_remove(cola_new, 0);
+    t_estado_metricas *metricas_new = list_get(pcb->metricas, 0);
+    temporal_stop(metricas_new->MT);
+    pcb->estado_actual = READY;
+    t_estado_metricas *ready = crear_metrica_estado(READY);
+    list_add(pcb->metricas, ready);
+    list_add(cola_ready, pcb);
 }
--- kernel/src/main.h
+++ kernel/src/main.h
@@ -6,7 +6,48 @@
 #include <pthread.h>
 #include <utils/config.h>
 #include <utils/log.h>
+#include <commons/temporal.h>
+#include <commons/collections/queue.h>
+#include <utils/estructuras.h>
 
-void escucha_io(t_config* config);
-void handshake_memoria(t_config*);
-void escucha_cpu(t_config* config);
+typedef enum {
+    NEW,
+    READY,
+    EXEC,
+    BLOCKED,
+    SUSP_BLOCKED,
+    SUSP_READY,
+    EXIT_STATUS
+} t_estado;
+
+typedef struct {
+    t_estado estado;
+    uint32_t ME;
+    t_temporal *MT;
+} t_estado_metricas;
+
+typedef struct {
+    uint32_t pid;
+    uint32_t pc;
+    t_list* metricas;
+    t_estado estado_actual;
+} t_pcb;
+
+void escucha_io();
+void handshake_memoria();
+void escucha_cpu();
+void largo_plazo(kernel_to_memoria*);
+void planificar_fifo(kernel_to_memoria*);
+t_estado_metricas *crear_metrica_estado(t_estado);
+t_pcb *crear_proceso(kernel_to_memoria*);
+bool consultar_a_memoria(uint32_t tamanio_proceso, uint32_t pid);
+void enviar_instrucciones(kernel_to_memoria* archivo);
+t_buffer *serializar_kernel_to_memoria(kernel_to_memoria* archivo);
+void pasar_ready();
+
+t_log *logger;
+t_config* config;
+uint32_t pid_counter = 0;
+t_list *cola_new;
+t_list *cola_ready;
+t_list *cola_blocked;
