4689ddf6775747c716a0ce50e2c9b0c0fe6b8572
alenodav <dgomezfe@frba.utn.edu.ar>
2025-06-20 19:19:38 +0000

    estructuras tablas paginas, metricas, y marcos

---
+++ memoria/include/marcos_libres.h
@@ -0,0 +1,12 @@
+#ifndef MARCOS_LIBRES_H_INCLUDED
+#define MARCOS_LIBRES_H_INCLUDED
+
+typedef struct{
+    uint32_t nroMarco;
+    bool estaLibre;
+} t_marco_libre;
+
+
+t_list* marcosLibres;
+
+#endif
---
+++ memoria/src/metricas.c
@@ -0,0 +1,8 @@
+#include <../include/metricas.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void mostrar_metricas(){
+    
+}
