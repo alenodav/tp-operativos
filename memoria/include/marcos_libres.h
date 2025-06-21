6e06999f8d587c55b9483269387eb47774a1c24b
TomasGiacoboni19 <tgiacoboni@frba.utn.edu.ar>
2025-06-21 19:57:53 +0000

    Agrego manejo de marcos libres

--- memoria/include/marcos_libres.h
+++ memoria/include/marcos_libres.h
@@ -1,12 +1,11 @@
 #ifndef MARCOS_LIBRES_H_INCLUDED
 #define MARCOS_LIBRES_H_INCLUDED
+#include <commons/collections/list.h>
+#include <stdint.h>
 
-typedef struct{
-    uint32_t nroMarco;
-    bool estaLibre;
-} t_marco_libre;
-
-
-t_list* marcosLibres;
-
+void inicializar_bitmap(int cantidad_frames);
+bool hay_marcos_libres();
+int obtener_marco_libre();
+void liberar_marco(int marco);
+void destruir_bitmap();
 #endif
